8d070f964c8faa4270024b75afd8608a76193c97
Domino987 <domino051@gmail.com>
2021-04-29 17:12:57 +0200

    Use alpha if possible
    V5 deprecated fade for alpha and this change starts the migration

--- src/components/Overlay/OverlayLoading.js
+++ src/components/Overlay/OverlayLoading.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CircularProgress, fade } from '@material-ui/core';
+import { CircularProgress, fade, alpha } from '@material-ui/core'; // TODO: v5 migration: remove fade for good
 
 function OverlayLoading(props) {
   return (
@@ -10,7 +10,7 @@ function OverlayLoading(props) {
         display: 'table',
         width: '100%',
         height: '100%',
-        backgroundColor: fade(props.theme.palette.background.paper, 0.7)
+        backgroundColor: (alpha ?? fade)(props.theme.palette.background.paper, 0.7)
       }}
     >
       <div
